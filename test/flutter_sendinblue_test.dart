8bf3076de4a478f8980c9964c8ffa692fbc7c295
Thomas Ecalle <thomasecalle@gmail.com>
2023-02-10 18:39:02 +0100

    Tests: Add some e2e tests

--- test/flutter_sendinblue_test.dart
+++ test/flutter_sendinblue_test.dart
@@ -12,4 +12,58 @@ void main() {
       expect(() => Sendinblue.instance, returnsNormally);
     });
   });
+
+  group('End to End tests', () {
+    const testUserEmail = 'test_user@gmail.com';
+
+    setUpAll(() async {
+      const apiKey = 'add_your_api_key_here';
+
+      Sendinblue.initialize(
+        configuration: SendinblueConfiguration(
+          apiKey: apiKey,
+        ),
+      );
+    });
+
+    test('Should create a contact', () async {
+      final contactId = await Sendinblue.instance.createContact(email: testUserEmail);
+      expect(contactId, isNotNull);
+    });
+
+    test('Should get a contact', () async {
+      final contact = await Sendinblue.instance.getContact(email: testUserEmail);
+      expect(contact, isNotNull);
+      expect(contact.email, testUserEmail);
+    });
+
+    test('Should update a contact', () async {
+      final contact = await Sendinblue.instance.getContact(email: testUserEmail);
+      expect(contact, isNotNull);
+      expect(contact.email, testUserEmail);
+
+      await Sendinblue.instance.updateContactProperties(
+        email: testUserEmail,
+        properties: {
+          'NOM': 'John',
+        },
+      );
+
+      final updatedContact = await Sendinblue.instance.getContact(email: testUserEmail);
+
+      expect(updatedContact, isNotNull);
+      expect(updatedContact.email, testUserEmail);
+      expect(updatedContact.attributes['NOM'], 'John');
+    });
+
+    test('Should delete a contact', () async {
+      final contact = await Sendinblue.instance.getContact(email: testUserEmail);
+      expect(contact, isNotNull);
+      expect(contact.email, testUserEmail);
+
+      await Sendinblue.instance.deleteContact(email: testUserEmail);
+
+      expect(() => Sendinblue.instance.getContact(email: testUserEmail), throwsA(isA<SendinblueApiException>()));
+    });
+  });
 }
