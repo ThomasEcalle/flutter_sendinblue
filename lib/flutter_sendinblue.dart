e8cefa18182cc0dda5df49d002b3cb1abfe7d3c3
Thomas Ecalle <thomasecalle@gmail.com>
2023-02-10 15:12:38 +0100

    Add some documentation comments

--- lib/flutter_sendinblue.dart
+++ lib/flutter_sendinblue.dart
@@ -12,6 +12,10 @@ export 'src/model/sendinblue_configuration.dart';
 class Sendinblue {
   static Sendinblue? _instance;
 
+  /// Initialize the Sendinblue package
+  ///
+  /// [apiKey] is the API key of your Sendinblue account
+  /// Note that this method should be called before using any other method of this package.
   static void initialize({
     required SendinblueConfiguration configuration,
   }) {
@@ -36,34 +40,48 @@ class Sendinblue {
 
   Sendinblue._(this._configuration, this._repository);
 
+  /// Create a contact based on [email].
   Future<int> createContact({required String email}) async {
     return _repository.createContact(email: email);
   }
 
+  /// Get all the contacts without any filter nor pagination.
   Future<List<Contact>> getAllContacts() async {
     return _repository.getAllContacts();
   }
 
+  /// Get contacts with pagination based on [offset] and [limit].
+  ///
+  /// getContacts(offset: 0, limit: 50) will return the first 50 contacts.
+  /// getContacts(offset: 50, limit: 50) will return the second 50 contacts.
+  /// etc.
   Future<List<Contact>> getContacts({int offset = 0, limit = 50}) async {
     return _repository.getContacts(offset: offset, limit: limit);
   }
 
+  /// Get a contact based on [email].
   Future<Contact> getContact({required String email}) async {
     return _repository.getContact(email: email);
   }
 
+  /// Delete a contact based on [email].
   Future<void> deleteContact({required String email}) async {
     return _repository.deleteContact(email: email);
   }
 
+  /// Add a contact in the emails black list based on [email].
   Future<void> addContactInEmailsBlackList({required String email}) async {
     return _repository.addContactInEmailsBlackList(email: email);
   }
 
+  /// Remove a contact from the emails black list based on [email].
   Future<void> removeContactFromEmailsBlackList({required String email}) async {
     return _repository.removeContactFromEmailsBlackList(email: email);
   }
 
+  /// Update a contact [properties] based on its [email].
+  ///
+  /// Note that the [properties] will only be handled by Sendinblue if they are already defined in your project.
   Future<void> updateContactProperties({required String email, required Map<String, dynamic> properties}) async {
     return _repository.updateContactProperties(email: email, properties: properties);
   }
